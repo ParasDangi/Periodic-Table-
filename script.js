c4c5d70715bb14209e0d1a7100d0ddfa2d1022d5
Paras Dangi <Parasdangi49@outlook.com>
2025-05-23 20:02:51 +0530

    Files arranged

---
+++ script.js
@@ -0,0 +1,361 @@
+
+function Hydrogen() {
+    window.open("https://www.britannica.com/science/hydrogen", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Helium() {
+    window.open("https://www.britannica.com/science/helium-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Lithium() {
+    window.open("https://www.britannica.com/science/lithium-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+
+function Beryllium() {
+    window.open("https://www.britannica.com/science/beryllium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Boron() {
+    window.open("https://www.britannica.com/science/boron-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Carbon() {
+    window.open("https://www.britannica.com/science/carbon-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Nitrogen() {
+    window.open("https://www.britannica.com/science/nitrogen", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Oxygen() {
+    window.open("https://www.britannica.com/science/oxygen", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Fluorine() {
+    window.open("https://www.britannica.com/science/fluorine", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Neon() {
+    window.open("https://www.britannica.com/science/neon-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Sodium() {
+    window.open("https://www.britannica.com/science/sodium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Magnesium() {
+    window.open("https://www.britannica.com/science/magnesium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Aluminum() {
+    window.open("https://www.britannica.com/science/aluminum", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Silicon() {
+    window.open("https://www.britannica.com/science/Silicon", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Phosphorus() {
+    window.open("https://www.britannica.com/science/phosphorus-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Sulfur() {
+    window.open("https://www.britannica.com/science/sulfur", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Chlorine() {
+    window.open("https://www.britannica.com/science/chlorine", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Argon() {
+    window.open("https://www.britannica.com/science/argon-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Potassium() {
+    window.open("https://www.britannica.com/science/potassium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Calcium() {
+    window.open("https://www.britannica.com/science/calcium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Scandium() {
+    window.open("https://www.britannica.com/science/scandium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Titanium() {
+    window.open("https://www.britannica.com/science/titanium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Vanadium() {
+    window.open("https://www.britannica.com/science/vanadium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Chromium() {
+    window.open("https://www.britannica.com/science/chromium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Manganese() {
+    window.open("https://www.britannica.com/science/manganese", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Iron() {
+    window.open("https://www.britannica.com/science/iron-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Cobalt() {
+    window.open("https://www.britannica.com/science/cobalt-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Nickel() {
+    window.open("https://www.britannica.com/science/nickel-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Copper() {
+    window.open("https://www.britannica.com/science/copper", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Zinc() {
+    window.open("https://www.britannica.com/science/zinc", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Gallium() {
+    window.open("https://www.britannica.com/science/gallium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Germanium() {
+    window.open("https://www.britannica.com/science/germanium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Arsenic() {
+    window.open("https://www.britannica.com/science/arsenic", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Selenium() {
+    window.open("https://www.britannica.com/science/selenium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Bromine() {
+    window.open("https://www.britannica.com/science/bromine", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Krypton() {
+    window.open("https://www.britannica.com/science/krypton-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Rubidium() {
+    window.open("https://www.britannica.com/science/rubidium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Strontium() {
+    window.open("https://www.britannica.com/science/strontium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Yttrium() {
+    window.open("https://www.britannica.com/science/yttrium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Zirconium() {
+    window.open("https://www.britannica.com/science/zirconium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Niobium() {
+    window.open("https://www.britannica.com/science/niobium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Molybdenum() {
+    window.open("https://www.britannica.com/science/molybdenum", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Technetium() {
+    window.open("https://www.britannica.com/science/technetium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Ruthenium() {
+    window.open("https://www.britannica.com/science/ruthenium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Rhodium() {
+    window.open("https://www.britannica.com/science/rhodium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Palladium() {
+    window.open("https://www.britannica.com/science/palladium-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Silver() {
+    window.open("https://www.britannica.com/science/silver", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Cadmium() {
+    window.open("https://www.britannica.com/science/cadmium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Indium() {
+    window.open("https://www.britannica.com/science/indium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Tin() {
+    window.open("https://www.britannica.com/science/tin", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Antimony() {
+    window.open("https://www.britannica.com/science/antimony", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function tellurium() {
+    window.open("https://www.britannica.com/science/tellurium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Iodine() {
+    window.open("https://www.britannica.com/science/iodine", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Xenon() {
+    window.open("https://www.britannica.com/science/xenon", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Cesium() {
+    window.open("https://www.britannica.com/science/cesium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Barium() {
+    window.open("https://www.britannica.com/science/barium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Lanthanum() {
+    window.open("https://www.britannica.com/science/lanthanum", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function cerium() {
+    window.open("https://www.britannica.com/science/cerium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Praseodymium() {
+    window.open("https://www.britannica.com/science/praseodymium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function neodymium() {
+    window.open("https://www.britannica.com/science/neodymium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Promethium() {
+    window.open("https://www.britannica.com/science/promethium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Samarium() {
+    window.open("https://www.britannica.com/science/samarium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Europium() {
+    window.open("https://www.britannica.com/science/europium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Gadolinium() {
+    window.open("https://www.britannica.com/science/gadolinium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Terbium() {
+    window.open("https://www.britannica.com/science/terbium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Dysprosium() {
+    window.open("https://www.britannica.com/science/dysprosium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Holmium() {
+    window.open("https://www.britannica.com/science/holmium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Erbium() {
+    window.open("https://www.britannica.com/science/erbium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Thulium() {
+    window.open("https://www.britannica.com/science/thulium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Ytterbium() {
+    window.open("https://www.britannica.com/science/ytterbium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Lutetium() {
+    window.open("https://www.britannica.com/science/lutelium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Hafnium() {
+    window.open("https://www.britannica.com/science/hafnium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Tantalum() {
+    window.open("https://www.britannica.com/science/tantalum", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Tungsten() {
+    window.open("https://www.britannica.com/science/tungsten-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Rhunium() {
+    window.open("https://www.britannica.com/science/rhunium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Osmium() {
+    window.open("https://www.britannica.com/science/osmium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Iridium() {
+    window.open("https://www.britannica.com/science/iridium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Platinum() {
+    window.open("https://www.britannica.com/science/platinum", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Gold() {
+    window.open("https://www.britannica.com/science/gold-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Mercury() {
+    window.open("https://www.britannica.com/science/mercury-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Thallium() {
+    window.open("https://www.britannica.com/science/thallium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Lead() {
+    window.open("https://www.britannica.com/science/lead-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Bismuth() {
+    window.open("https://www.britannica.com/science/bismuth", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Polonium() {
+    window.open("https://www.britannica.com/science/polonium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Astatine() {
+    window.open("https://www.britannica.com/science/astatine", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Radon() {
+    window.open("https://www.britannica.com/science/radon", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Francium() {
+    window.open("https://www.britannica.com/science/francium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Radium() {
+    window.open("https://www.britannica.com/science/radium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Actinium() {
+    window.open("https://www.britannica.com/science/actinium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Thorium() {
+    window.open("https://www.britannica.com/science/thorium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Protactinium() {
+    window.open("https://www.britannica.com/science/protactinium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Uranium() {
+    window.open("https://www.britannica.com/science/uranium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Neptunium() {
+    window.open("https://www.britannica.com/science/neptunium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Plutonium() {
+    window.open("https://www.britannica.com/science/plutonium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Americium() {
+    window.open("https://www.britannica.com/science/americium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Curium() {
+    window.open("https://www.britannica.com/science/curium-chemical-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Berkelium() {
+    window.open("https://www.britannica.com/science/berkelium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Californium() {
+    window.open("https://www.britannica.com/science/califorium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Einsteinium() {
+    window.open("https://www.britannica.com/science/einteinium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Fermium() {
+    window.open("https://www.britannica.com/science/fermium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Mendelevium() {
+    window.open("https://www.britannica.com/science/mendelevium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Nobelium() {
+    window.open("https://www.britannica.com/science/nobelium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Lawrencium() {
+    window.open("https://www.britannica.com/science/lawrencium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Rutherfordium() {
+    window.open("https://www.britannica.com/science/rutherfordium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Dubnium() {
+    window.open("https://www.britannica.com/science/dubnium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Seaborgium() {
+    window.open("https://www.britannica.com/science/seaborgium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Bohrium() {
+    window.open("https://www.britannica.com/science/bohrium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Hassium() {
+    window.open("https://www.britannica.com/science/hassium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Meitnerium() {
+    window.open("https://www.britannica.com/science/meitnerium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Darmstadtium() {
+    window.open("https://www.britannica.com/science/darmstadtium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Copericium() {
+    window.open("https://www.britannica.com/science/copericium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Nihonium() {
+    window.open("https://www.britannica.com/science/element-113", "myWindow", "width=720,height=1000,top=100,left=400");
+} function Flerovium() {
+    window.open("https://www.britannica.com/science/flerovium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Mosocovium() {
+    window.open("https://www.britannica.com/science/element-115", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Livermorium() {
+    window.open("https://www.britannica.com/science/livermorium", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Tennessine() {
+    window.open("https://www.britannica.com/science/element-117", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Oganesson() {
+    window.open("https://www.britannica.com/science/element-118", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Lanthanoid() {
+    window.open("https://www.britannica.com/science/lanthanoid", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Actnoid() {
+    window.open("https://www.britannica.com/science/actinoid-element", "myWindow", "width=720,height=1000,top=100,left=400");
+}
+function Summary() {
+    window.open("https://www.britannica.com/summary/periodic-table", "myWindow", "width=720,height=1000,top=100,left=400");
+}
